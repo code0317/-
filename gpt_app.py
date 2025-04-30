5ddbad5005a0ba7e1e4e8c461590023561219acf
lijukwang <lhsagape2816@gmail.com>
2025-04-30 15:42:53 +0900

    Update gpt_app.py

--- gpt_app.py
+++ gpt_app.py
@@ -1,2 +1,15 @@
-name = st.text_input("OpenAI API Key")
-st.text(f"apikey :{name}")
+import streamlit as st
+from openai import OpenAI
+
+api_key= st.text_input("OpenAI API Key", type="password")
+client = OpenAI(api_key=api_key)
+
+st.title("OpenAI GPT model")
+
+prompt = st.text_area("User prompt")
+
+if st.button("Ask!", disabled=(len(prompt)==0)):
+    response = client.responses.create(
+        model="gpt-4.1-mini",
+        input=prompt
+    )
