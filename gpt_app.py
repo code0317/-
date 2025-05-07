d4385d340ec94b2f4db0679e7828f633e4d5c05a
lijukwang <lhsagape2816@gmail.com>
2025-05-07 14:32:19 +0900

    Update gpt_app.py

--- gpt_app.py
+++ gpt_app.py
@@ -1,16 +1,26 @@
 import streamlit as st
 from openai import OpenAI
 
-api_key= st.text_input("OpenAI API Key", type="password")
-client = OpenAI(api_key=api_key)
+if "api_key" not in st.session_state:
+    st.session_state.api_key = ""
 
-st.title("OpenAI GPT model")
+api_key = st.text_input("OpenAI API Key", type="password", value=st.session_state.api_key)
 
-prompt = st.text_area("User prompt")
+if api_key != st.session_state.api_key:
+    st.session_state.api_key = api_key
+    
+if st.session_state.api_key:
+    client = OpenAI(api_key=st.session_state.api_key)
+    
+    st.title("OpenAI GPT model")
+    
+    prompt = st.text_area("User prompt")
 
-if st.button("Ask!", disabled=(len(prompt)==0)):
+    if st.button("Ask!", disabled=(len(prompt)==0)):
     response = client.responses.create(
         model="gpt-4.1-mini",
         input=prompt
     )
     st.write(response.output_text)
+
+else:st.warning("API Key를 입력해 주세요.") 
